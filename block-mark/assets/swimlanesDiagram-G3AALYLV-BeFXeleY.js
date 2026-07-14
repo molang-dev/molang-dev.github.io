e0198e45ba9a8bd06a3f6d592b266312dab1cee7
molang-dev <molang.dev@gmail.com>
2026-07-14 13:03:51 +0800

    block-mark demo preview

---
+++ block-mark/assets/swimlanesDiagram-G3AALYLV-BeFXeleY.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-SGRTOZ0-.js";import{_ as a}from"./index-BK6fa0Ff.js";import"./chunk-5VM5RSS4-B_f1kYbR.js";import"./chunk-XXDRQBXY-BrGWshKP.js";import"./chunk-VR4S4FIN-DINcdPqv.js";import"./chunk-32BRIVSS-Dt8motBU.js";import"./channel-ClPqaykN.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
